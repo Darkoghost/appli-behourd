78cc03eb5163de1b923cbb123ba12d6f2e32ca32
DarkoGhost <jujuiolm@hotmail.fr>
2021-10-11 11:37:48 +0200

    Premier test par Julien et Eva

---
+++ partie_1c1.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert')
+var sinon = require('sinon')
+
+// Given 2 players WHEN want to play THEN Launch a game in 1V1
+describe('FaireUnePartieEn1c1', function()
+{
+    it('La partie doit commencer', function(){
+        let equipe1 = new Equipe();
+        let equipe2 = new Equipe();
+        let joueur1 = new Joueur();
+        let joueur2 = new Joueur();
+        equipe1.collectionjoueur.add(joueur1);
+        equipe2.collectionjoueur.add(joueur2);
+        let partie = new Partie(equipe1,equipe2);
+    })
+})
