9e2a068cb8ea06f83e1c87666701a51e2c3e71b9
eva.guegan <eva.guegan@hotmail.fr>
2021-10-11 16:57:47 +0200

    mise à jour test by eva & julien

--- test/partie_1c1.test.js
+++ test/partie_1c1.test.js
@@ -3,18 +3,38 @@ var sinon = require('sinon')
 var Equipe = require('../app/Equipe.js')
 var Combattant = require('../app/Combattant.js')
 
-// Given 2 players WHEN want to play THEN Launch a game in 1V1
-describe('FaireUneCombatEn1c1', function()
+// Given 2 players WHEN session start THEN Launch a game in 1V1
+describe('FaireUnCombatEn1c1', function()
 {
     it('La partie doit commencer', function(){
-        let equipe1 = new Equipe();
-        let equipe2 = new Equipe();
-        let joueur1 = new Combattant("GROS","Paul",134,1,2006);
-        let joueur2 = new Combattant("MAIGRE","Louis",134,1,2012);
-        equipe1.add(joueur1);
-        equipe2.add(joueur2);
-        assert.equals(equipe1.collectionjoueur.length(),equipe2.collectionjoueur.length());
-        assert.notEqual(joueur1,joueur2);
+
+        // 1- Création de la session
+        let session = new Session();
+        // 2- Récupération des combattants
+        let combattant1 = new Combattant("GROS","Paul",134,1,2006);
+        let combattant2 = new Combattant("MAIGRE","Louis",134,1,2012);
+
+        // 3- Ajout de tous les combattants dans la session
+        session.addCombattant(combattant1, combattant2);
+        // 4- Création d'un combat
+        session.createCombat(new Combat());
+
+        // 5- Création des équipes
+        let combat = session.combat;
+        combat.createEquipe();
+
+        // 6- Répartition des combattants dans les équipes
+        let equipes = combat.equipes;
+        equipes[0].addCombattant(combattant1);
+        equipes[1].addCombattant(combattant2);
+
+        // assert.equals(equipe1.collectioncombattant.length(),equipe2.collectioncombattant.length());
+        // assert.notEqual(combattant1,combattant2);
+
+        Assert.True(equipes.Length === 2);
+        // Assert.True(equipes.All(equipe => equipe.PlayerCount === 1));
+        Assert.deepEqual(combattant1, combattant2);
+        // assert.equals(JSON.stringify(combattant1), JSON.stringify(combattant2));
     })
 })
 
@@ -22,14 +42,14 @@ describe('FaireUneCombatEn1c1', function()
 it('La partie doit commencer', function(){
     let equipe1 = new Equipe();
     let equipe2 = new Equipe();
-    let joueur1 = new Combattant("GROS","Paul",134,1,2006);
-    let joueur2 = new Combattant("MAIGRE","Louis",134,1,2012);
-    let joueur3 = new Combattant("PETIT","Jean",134,1,2001);
-    let joueur4 = new Combattant("GRAND","Jacque",134,1,2010);
-    let joueur5 = new Combattant("GROS","Paul",134,0,0);
-    let joueur6 = new Combattant("GROS","Paul",134,0,0);
-    equipe1.combattant.add(joueur1);
-    equipe2.combattant.add(joueur2);
+    let combattant1 = new Combattant("GROS","Paul",134,1,2006);
+    let combattant2 = new Combattant("MAIGRE","Louis",134,1,2012);
+    let combattant3 = new Combattant("PETIT","Jean",134,1,2001);
+    let combattant4 = new Combattant("GRAND","Jacque",134,1,2010);
+    let combattant5 = new Combattant("GROS","Paul",134,0,0);
+    let combattant6 = new Combattant("GROS","Paul",134,0,0);
+    equipe1.combattant.add(combattant1);
+    equipe2.combattant.add(combattant2);
     let Combat = new Combat(equipe1,equipe2);
-    assert.equals(equipe1.collectionjoueur.length(),equipe2.collectionjoueur.length() )
+    assert.equals(equipe1.collectioncombattant.length(),equipe2.collectioncombattant.length() )
 })*/
