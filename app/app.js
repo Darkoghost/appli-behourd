eb3cb8cfbc5f64c6765aa9c42eebfa0312c1f0ef
eva.guegan <eva.guegan@hotmail.fr>
2021-10-13 15:33:23 +0200

    mise à jour todo list

--- app/app.js
+++ app/app.js
@@ -1,15 +1,3 @@
-
-
-let equipe1 = new Equipe();
-let equipe2 = new Equipe();
-let joueur1 = new Combattant("GROS","Paul",134,1,2006);
-let joueur2 = new Combattant("MAIGRE","Louis",134,1,2012);
-
-// 1- Création de la session
-let session = new Session();
-// 2- Récupération des combattants
-let combattant1 = new Combattant.constructor("GROS","Paul",134,1,2006);
-let combattant2 = new Combattant.constructor("MAIGRE","Louis",134,1,2012);
-
-// 3- Ajout de tous les combattants dans la session
-session.addCombattants(combattant1, combattant2);
+// OK TODO de eva : faire test avec 3 joueurs pour les trier avec le poids
+// TODO de juju : faire test quand des joueurs veulent rejoindre la session en cours
+// TODO amélioration : rassembler le code de trie du nombre des joueurs et de leurs poids
